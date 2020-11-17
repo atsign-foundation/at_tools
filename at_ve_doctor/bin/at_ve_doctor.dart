9cd2d98fd958749541539f86a7bbc1971d29daf1
cconstab <colin@constable.cc>
2020-11-16 21:10:29 -0800

    Virtual Enviroment doctor
    Small demo/tool to check that your virtual environment is up and working working and that all the @signs are ready and activated..

---
+++ at_ve_doctor/bin/at_ve_doctor.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:at_server_status/at_server_status.dart';
+
+void main() async {
+
+  var atSigns = [ '@alice🛠','@ashish🛠','@barbara🛠','@bob🛠','@colin🛠','@egbiometric🛠','@egcovidlab🛠','@egcreditbureau🛠','@eggovagency🛠','@emoji🦄🛠','@eve🛠','@jagan🛠','@kevin🛠','@murali🛠','@naresh🛠','@purnima🛠','@sameeraja🛠','@sitaram🛠'];
+
+  Future<AtStatus> getAtStatus(atSign) async {
+    AtStatus atStatus;
+    AtStatusImpl atStatusImpl;
+    atStatusImpl = AtStatusImpl();
+
+    atStatusImpl.rootUrl = 'test.do-sf2.atsign.zone';
+    atStatus = await atStatusImpl.get(atSign);
+    print('${atSign} status: ${atStatus.status()}');
+    return atStatus;
+  }
+
+  await Future.forEach(atSigns, (element) async {
+    await getAtStatus(element);
+  });
+
+  exit(0);
+}
