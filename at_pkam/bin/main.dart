36cb44b10d32361c2c1f0500304380bb5daac66d
nareshg <naresh0689@gmail.com>
2021-02-15 07:35:56 +0530

    changes to read keys zip file

--- at_pkam/bin/main.dart
+++ at_pkam/bin/main.dart
@@ -3,6 +3,7 @@ import 'package:encrypt/encrypt.dart';
 import 'package:crypton/crypton.dart';
 import 'dart:io';
 import 'package:at_pkam/pkam_constants.dart';
+import 'package:archive/archive_io.dart';
 
 Future<void> main(List<String> arguments) async {
   var filePath = arguments[0];
@@ -10,8 +11,11 @@ Future<void> main(List<String> arguments) async {
   var privateKey;
   if (filePath.endsWith(AT_KEYS)) {
     privateKey = await getSecretFromAtKeys(filePath);
+  } else if (filePath.endsWith(ZIP)) {
+    privateKey = await getSecretFromZip(filePath);
   } else {
-    throw Exception('Please provide .keys file as first argument');
+    throw Exception(
+        'Provide .keys or zip file. \n Syntax : dart main.dart <.keys/zip file path> <from_response>');
   }
   privateKey = privateKey.trim();
   var key = RSAPrivateKey.fromString(privateKey);
@@ -34,6 +38,32 @@ Future<String> getSecretFromAtKeys(String filePath) async {
     var aesEncryptionKey = keysJSON['selfEncryptionKey'];
     var pkamPrivateKey =
         decryptValue(encryptedPKAMPrivateKey, aesEncryptionKey);
+    return pkamPrivateKey;
+  } on Exception catch (e) {
+    print('Exception while getting secret : ${e}');
+  }
+}
+
+Future<String> getSecretFromZip(String filePath) async {
+  try {
+    var isFileExists = await File(filePath).exists();
+    if (!isFileExists) {
+      throw Exception('File not found');
+    }
+    var fileContents;
+    var bytes = File(filePath).readAsBytesSync();
+    final archive = ZipDecoder().decodeBytes(bytes);
+    for (var file in archive) {
+      if (file.name.contains('atKeys')) {
+        fileContents = String.fromCharCodes(file.content);
+      }
+    }
+    var keysJSON = json.decode(fileContents);
+    var encryptedPKAMPrivateKey = keysJSON['aesPkamPrivateKey'];
+    print('Please scan QR code image and provide aesKey');
+    var aesKey = stdin.readLineSync();
+    aesKey = aesKey.trim();
+    var pkamPrivateKey = decryptValue(encryptedPKAMPrivateKey, aesKey);
     return pkamPrivateKey;
   } on Exception catch (e) {
     print('Exception while getting secret : ${e}');
