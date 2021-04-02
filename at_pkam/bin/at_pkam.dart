23dddc4673dc89830554a590191284f2cbec0310
sita ram kalluri <kalluriramkumar@gmail.com>
2021-04-02 17:01:16 +0530

    add github actions

--- at_pkam/bin/at_pkam.dart
+++ at_pkam/bin/at_pkam.dart
@@ -1,5 +1,7 @@
-import 'package:crypton/crypton.dart';
+import 'dart:convert';
 import 'dart:io';
+
+import 'package:crypton/crypton.dart';
 import 'package:path/path.dart';
 
 void main(List<String> arguments) {
@@ -9,8 +11,8 @@ void main(List<String> arguments) {
   var key = RSAPrivateKey.fromString(privateKey);
   var challenge = stdin.readLineSync();
   challenge = challenge.trim();
-  var signature = key.createSignature(challenge);
-
+  var signature =
+      base64.encode(key.createSHA256Signature(utf8.encode(challenge)));
   stdout.write(signature);
   stdout.write('\n');
 }
