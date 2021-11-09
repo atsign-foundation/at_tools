c3f8fb0140834412c5fc17b38d0d5ea06fcc9004
murali-shris <murali.25@gmail.com>
2021-11-09 20:02:37 +0530

    usage fix

--- at_hive_recovery/bin/main.dart
+++ at_hive_recovery/bin/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:at_hive_recovery/src/hive_doctor.dart';
 
 void main(var args) async {
   if (args == null || args.length < 2) {
     print('usage: dart bin/main dart <atsign> <dir_path_of_hive_box>');
+    exit(0);
   }
   final atSign = args[0];
   final hiveStorageDir = args[1];
