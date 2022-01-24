21e1adf41be731786542cf311aa5c1e7dbe92eee
sitaram <kalluriramkumar@gmail.com>
2022-01-24 12:18:18 +0530

    changes for backward compactability

--- at_utils/lib/src/atsign_util.dart
+++ at_utils/lib/src/atsign_util.dart
@@ -73,7 +73,7 @@ class AtUtils {
   }
 
   /// Return AtSign by appending '@' at the beginning if not present
-  static String formatAtSign(String? atSign) {
+  static String? formatAtSign(String? atSign) {
     // verify whether atSign started with '@' or not
     if (atSign == null) {
       return '';
