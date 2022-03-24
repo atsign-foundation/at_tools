a20624e9e4a5b0adc4dbcfa0939077ed78768591
sitaram <kalluriramkumar@gmail.com>
2022-03-24 16:07:59 +0530

    Notifications to have same id across

--- at_commons/test/notify_verb_builder_test.dart
+++ at_commons/test/notify_verb_builder_test.dart
@@ -5,27 +5,30 @@ void main() {
   group('A group of notify verb builder tests to check notify command', () {
     test('notify public key', () {
       var notifyVerbBuilder = NotifyVerbBuilder()
+        ..id = '123'
         ..value = 'alice@gmail.com'
         ..isPublic = true
         ..atKey = 'email'
         ..sharedBy = 'alice';
       expect(notifyVerbBuilder.buildCommand(),
-          'notify:notifier:SYSTEM:public:email@alice:alice@gmail.com\n');
+          'notify:id:123:notifier:SYSTEM:public:email@alice:alice@gmail.com\n');
     });
 
     test('notify public key with ttl', () {
       var notifyVerbBuilder = NotifyVerbBuilder()
+        ..id = '123'
         ..value = 'alice@gmail.com'
         ..isPublic = true
         ..atKey = 'email'
         ..sharedBy = 'alice'
         ..ttl = 1000;
       expect(notifyVerbBuilder.buildCommand(),
-          'notify:notifier:SYSTEM:ttl:1000:public:email@alice:alice@gmail.com\n');
+          'notify:id:123:notifier:SYSTEM:ttl:1000:public:email@alice:alice@gmail.com\n');
     });
 
     test('notify shared key command', () {
       var notifyVerbBuilder = NotifyVerbBuilder()
+        ..id = '123'
         ..value = 'alice@atsign.com'
         ..atKey = 'email'
         ..sharedBy = 'alice'
@@ -33,7 +36,7 @@ void main() {
         ..pubKeyChecksum = '123'
         ..sharedKeyEncrypted = 'abc';
       expect(notifyVerbBuilder.buildCommand(),
-          'notify:notifier:SYSTEM:sharedKeyEnc:abc:pubKeyCS:123:@bob:email@alice:alice@atsign.com\n');
+          'notify:id:123:notifier:SYSTEM:sharedKeyEnc:abc:pubKeyCS:123:@bob:email@alice:alice@atsign.com\n');
     });
   });
 }
