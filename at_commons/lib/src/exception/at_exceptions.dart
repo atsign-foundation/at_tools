84d6e3c05f3bc62a90c385ea6d8c40a1816279db
Jagannadh <jagannadh@Jagannadhs-MacBook-Air.local>
2022-04-18 16:33:42 +0530

    Reordered the classes a bit and added TBD to the ones the needs some more thought

--- at_commons/lib/src/exception/at_exceptions.dart
+++ at_commons/lib/src/exception/at_exceptions.dart
@@ -15,60 +15,19 @@ class AtException implements Exception {
   }
 }
 
-/// The class [AtConnectException] and its subclasses represent any connection related issues while
-/// trying to establish a socket connection to root or secondary server
+/// The class [AtConnectException] and its subclasses represent any issues that prevents an connection to the root or the secondary server
 class AtConnectException extends AtException {
   AtConnectException(message) : super(message);
 }
 
-/// Exception thrown when there is any issue related to socket operations e.g read/write
-class AtIOException extends AtException {
-  AtIOException(message) : super(message);
+/// Exception thrown when there is an issue connecting to root server
+class RootServerConnectivityException extends AtConnectException {
+  RootServerConnectivityException(message) : super(message);
 }
 
-/// Exception thrown for critical issue during secondary or root server startup operation
-@Deprecated('use at_server_exceptions.AtServerException')
-class AtServerException extends AtException {
-  AtServerException(message) : super(message);
-}
-
-/// Exception thrown when an @ protocol verb has an invalid syntax.
-class InvalidSyntaxException extends AtException {
-  InvalidSyntaxException(message) : super(message);
-}
-
-/// Exception thrown when an atsign name provided is invalid.
-class InvalidAtSignException extends AtException {
-  InvalidAtSignException(message) : super(message);
-}
-
-/// Thrown if the atsign is in blocklist.
-@Deprecated('use at_server_exceptions.BlockedConnectionException')
-class BlockedConnectionException extends AtException {
-  BlockedConnectionException(message) : super(message);
-}
-
-/// Exception thrown when data size passed to the socket is greater than configured buffer size
-class BufferOverFlowException extends AtException {
-  BufferOverFlowException(message) : super(message);
-}
-
-/// Thrown when lookup fails after handshake
-@Deprecated('use at_server_exceptions.LookupException')
-class LookupException extends AtException {
-  LookupException(message) : super(message);
-}
-
-/// Thrown for any unhandled server exception
-@Deprecated('use at_server_exceptions.InternalServerException')
-class InternalServerException extends AtException {
-  InternalServerException(message) : super(message);
-}
-
-/// Thrown for any unhandled server error
-@Deprecated('use at_server_exceptions.InternalServerError')
-class InternalServerError extends AtException {
-  InternalServerError(message) : super(message);
+/// Exception thrown when there is an issue connecting to secondary server
+class SecondaryServerConnectivityException extends AtConnectException {
+  SecondaryServerConnectivityException(message) : super(message);
 }
 
 /// Exception thrown when a client tries to execute a verb or establish a connection but doesn't have the required permissions
@@ -79,11 +38,6 @@ class UnAuthorizedException extends AtConnectException {
 /// Exception thrown when the requested atsign's secondary url is not present in Root server
 class SecondaryNotFoundException extends AtConnectException {
   SecondaryNotFoundException(message) : super(message);
-}
-
-/// Exception thrown when an atsign's secondary url cannot be reached or is unavailable
-class SecondaryConnectException extends AtException {
-  SecondaryConnectException(message) : super(message);
 }
 
 /// Exception thrown when from-pol handshake fails between two secondary servers
@@ -118,6 +72,67 @@ class OutBoundConnectionInvalidException extends AtConnectException {
   OutBoundConnectionInvalidException(message) : super(message);
 }
 
+/// Exception thrown when security certification validation on root or secondary server fails
+class AtCertificateValidationException extends AtException {
+  AtCertificateValidationException(message) : super(message);
+}
+
+/// Exception thrown when there is any issue related to socket operations e.g read/write
+class AtIOException extends AtException {
+  AtIOException(message) : super(message);
+}
+
+/// Exception thrown for critical issue during secondary or root server startup operation
+@Deprecated('use at_server_exceptions.AtServerException')
+class AtServerException extends AtException {
+  AtServerException(message) : super(message);
+}
+
+/// Exception thrown when an @ protocol verb has an invalid syntax.
+class InvalidSyntaxException extends AtException {
+  InvalidSyntaxException(message) : super(message);
+}
+
+/// Exception thrown when an atsign name provided is invalid.
+class InvalidAtSignException extends AtException {
+  InvalidAtSignException(message) : super(message);
+}
+
+/// Thrown if the atsign is in block list.
+@Deprecated('use at_server_exceptions.BlockedConnectionException')
+class BlockedConnectionException extends AtException {
+  BlockedConnectionException(message) : super(message);
+}
+
+/// Exception thrown when data size passed to the socket is greater than configured buffer size
+class BufferOverFlowException extends AtException {
+  BufferOverFlowException(message) : super(message);
+}
+
+/// Thrown when lookup fails after handshake
+@Deprecated('use at_server_exceptions.LookupException')
+class LookupException extends AtException {
+  LookupException(message) : super(message);
+}
+
+/// Thrown for any unhandled server exception
+@Deprecated('use at_server_exceptions.InternalServerException')
+class InternalServerException extends AtException {
+  InternalServerException(message) : super(message);
+}
+
+/// Thrown for any unhandled server error
+@Deprecated('use at_server_exceptions.InternalServerError')
+class InternalServerError extends AtException {
+  InternalServerError(message) : super(message);
+}
+
+/// Exception thrown when an atsign's secondary url cannot be reached or is unavailable
+/// Should this be extending AtConnectException?
+class SecondaryConnectException extends AtException {
+  SecondaryConnectException(message) : super(message);
+}
+
 /// Exception thrown when [AtKey.key] is not present in the keystore
 class KeyNotFoundException extends AtException {
   KeyNotFoundException(message) : super(message);
@@ -133,15 +148,6 @@ class AtTimeoutException extends AtException {
   AtTimeoutException(message) : super(message);
 }
 
-/// Exception thrown when there is an issue connecting to root server
-class RootServerConnectivityException extends AtConnectException {
-  RootServerConnectivityException(message) : super(message);
-}
-
-/// Exception thrown when there is an issue connecting to secondary server
-class SecondaryServerConnectivityException extends AtConnectException {
-  SecondaryServerConnectivityException(message) : super(message);
-}
 
 /// Exception thrown when request to secondary server is invalid
 class InvalidRequestException extends AtException {
@@ -153,7 +159,4 @@ class InvalidResponseException extends AtException {
   InvalidResponseException(message) : super(message);
 }
 
-/// Exception thrown when security certification validation on root or secondary server fails
-class AtCertificateValidationException extends AtException {
-  AtCertificateValidationException(message) : super(message);
-}
+
