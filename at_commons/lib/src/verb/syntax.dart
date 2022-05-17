8e6df076f8071b5567064fa68d26bc07fae9bb39
srieteja <srit1505@gmail.com>
2022-05-18 01:47:51 +0530

    new regex added for set verb

--- at_commons/lib/src/verb/syntax.dart
+++ at_commons/lib/src/verb/syntax.dart
@@ -11,6 +11,7 @@ class VerbSyntax {
       r'^lookup:((?<operation>meta|all):)?(?<atKey>(?:[^:]).+)@(?<atSign>[^@\s]+)$';
   static const scan =
       r'^scan$|scan(:(?<forAtSign>@[^:@\s]+))?(:page:(?<page>\d+))?( (?<regex>\S+))?$';
+  static const set = r'^set:$*';
   static const config =
       r'^config:block:(?<operation>add|remove|show)(?:(?<=show)\s?$|(?:(?<=add|remove):(?<atSign>(?:@[^\s@]+)( (?:@[^\s@]+))*$)))';
   static const stats =
