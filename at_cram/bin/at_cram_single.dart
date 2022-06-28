0724d3f3a1c5d46a5ee43d97e97886b7164fbe97
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-06-28 16:23:38 -0400

    Use `///` for comments

--- at_cram/bin/at_cram_single.dart
+++ at_cram/bin/at_cram_single.dart
@@ -2,19 +2,19 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:crypto/crypto.dart';
 
-// command-line usage
+/// command-line usage
 
-// (1)
-// first compile this file (replace <name> with the name of the file)
-// dart compile exe bin/at_cram_single.dart -o <name>
+/// (1)
+/// first compile this file (replace <name> with the name of the file)
+/// dart compile exe bin/at_cram_single.dart -o <name>
 
-// (2)
-// run the compiled file
-// ./cram <cramSecret> <from: challenge>
+/// (2)
+/// run the compiled file, <cramSecret> comes from initial retrieval of your atSign (in your QR code or web API), <challenge> comes from the `from:@sign` challenge.
+/// ./cram -k <cramSecret> -c <challenge>
 
-// (3)
-// program spits out digest
-// in openssl, you can now do `cram:<digest>`
+/// (3)
+/// program spits out digest
+/// in openssl, you can now do `cram:<digest>`
 
 void main(List<String> arguments) {
   if (arguments.length != 2) {
