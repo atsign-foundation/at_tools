66e23eee255a1db15d6ff36f5bbfb51d63cd2400
srieteja <srit1505@gmail.com>
2022-10-06 04:05:06 +0530

    fix: issue getting config.yaml path

--- at_cli/lib/src/config_util.dart
+++ at_cli/lib/src/config_util.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
+
 import 'package:at_utils/at_utils.dart';
 import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
 
 class ConfigUtil {
   static final ApplicationConfiguration appConfig =
-      ApplicationConfiguration('config/config.yaml');
+      ApplicationConfiguration(_getConfigDirectory());
 
   static YamlMap? getYaml() {
     return appConfig.getYaml();
+  }
+
+  static String _getConfigDirectory() {
+    if (!File('config/config.yaml').existsSync()) {
+      return path.join(Directory.current.parent.path, 'config/config.yaml');
+    }
+    return path.join(Directory.current.path, 'config/config.yaml');
   }
 }
