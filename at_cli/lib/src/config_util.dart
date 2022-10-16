fcd3967a75042a23dbf024fd8416a2f4baefa391
srieteja <srit1505@gmail.com>
2022-10-17 02:46:23 +0530

    fix: issue getting config.yaml path when main.dart is accessed globally

--- at_cli/lib/src/config_util.dart
+++ at_cli/lib/src/config_util.dart
@@ -1,11 +1,29 @@
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:at_utils/at_utils.dart';
 import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
 
 class ConfigUtil {
-  static final ApplicationConfiguration appConfig =
-      ApplicationConfiguration('config/config.yaml');
+  static late final ApplicationConfiguration appConfig;
 
+  static Future<void> init() async {
+      String configPath = await _getConfigFile();
+      appConfig = ApplicationConfiguration(configPath);
+  }
   static YamlMap? getYaml() {
     return appConfig.getYaml();
+  }
+
+  static Future<String> _getConfigFile() async {
+    var fileUri = await Isolate.resolvePackageUri(
+        Uri.parse('package:at_cli/'));
+    if (fileUri == null) {
+      throw Exception('Could not file config file at config/config.yaml');
+    }
+    Directory packageRoot = Directory.fromUri(fileUri).parent;
+    return path.join(packageRoot.path, 'config/config.yaml');
+
   }
 }
