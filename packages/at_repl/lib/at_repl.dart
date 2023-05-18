327350bb8d6efd61aa8a751596f502a36c7b0881
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-18 19:38:51 -0400

    feat: `at_repl` repl lib start

--- packages/at_repl/lib/at_repl.dart
+++ packages/at_repl/lib/at_repl.dart
@@ -1,1 +1,55 @@
-Future<dynamic> executeCommand(String command) {}
+
+
+
+import 'package:at_client/at_client.dart';
+import 'package:at_onboarding_cli/at_onboarding_cli.dart';
+import 'package:at_repl/home_directory.dart';
+
+class REPL {
+  final String atSign;
+  late AtOnboardingService _atOnboardingService;
+
+  REPL(this.atSign);
+
+  Future<bool> authenticate() {
+    AtOnboardingPreference pref = AtOnboardingPreference()
+    ..isLocalStoreRequired = true
+    ..hiveStoragePath = '${getHomeDirectory()}/.atsign/temp/hive'
+    ..commitLogPath = '${getHomeDirectory()}/.atsign/temp/commitlog'
+    ..downloadPath = '${getHomeDirectory()}/.atsign/temp/download'
+    ..namespace = 'soccer0'
+    ..atKeysFilePath = "${getHomeDirectory()}/.atsign/keys/${atSign}_key.atKeys";
+
+    _atOnboardingService = AtOnboardingServiceImpl(atSign, pref);
+
+    return _atOnboardingService.authenticate();
+  }
+
+  Future<String> executeCommand(String command) async {
+    late String result;
+    if(_atOnboardingService == null) {
+      throw Exception('Not authenticated. Call `authenticate()` first.');
+    }
+    if(_atOnboardingService.atClient == null) {
+      throw Exception('AtClient is null for some reason...');
+    }
+    if(_atOnboardingService.atClient!.getRemoteSecondary() == null) {
+      throw Exception('RemoteSecondary is null for some reason...');
+    }
+    final AtClient atClient = _atOnboardingService.atClient!;
+    for (var atKey in (await atClient.getAtKeys())) {
+      // print(atKey);
+    }
+    final RemoteSecondary rs = _atOnboardingService.atClient!.getRemoteSecondary()!;
+
+    final String? response = (await rs.executeCommand(command, auth: true));
+
+    if(response == null) {
+      throw Exception('Result is null for some reason after executing command: $command');
+    }
+
+    result = response;
+
+    return result;
+  }
+}
--- packages/at_repl/test/at_repl_test.dart
+++ packages/at_repl/test/at_repl_test.dart
@@ -1,8 +1,12 @@
-import 'package:at_repl/at_repl.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
+import 'package:at_repl/at_repl.dart';
+
+void main() async {
+  final String atSign = '@jeremy_0';
+  REPL repl = REPL(atSign);
+  final bool authenticated = await repl.authenticate();
+  print('authentication success: $authenticated');
+  final String result = await repl.executeCommand('scan');
+  print('scan outputed: $result');
 }
